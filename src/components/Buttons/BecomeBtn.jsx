e15ae65dc3f261e1706bac1c406318315be5336b
amnesia2k <tilewaolatoye17@gmail.com>
2024-10-09 08:43:08 +0100

    made button components

---
+++ src/components/Buttons/BecomeBtn.jsx
@@ -0,0 +1,14 @@
+function BecomeBtn() {
+  return (
+    <div>
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSc0mNuQcV98o0yqwwH3NkCKMS0b7T-kgoYZA3Bl5d_bQriFxQ/viewform"
+        className="bg-secondary font-inter py-2 px-5 rounded-lg text-white underline"
+      >
+        Become a Facilitator
+      </a>
+    </div>
+  );
+}
+
+export default BecomeBtn;
---
+++ src/components/Buttons/JoinBtnWhite.jsx
@@ -0,0 +1,14 @@
+function JoinBtnWhite() {
+  return (
+    <div>
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSfK8oBxORmK6-uGao7koKiYdg802bzz-ngbgID2LwX_8g26KQ/viewform"
+        className="font-inter py-2 px-5 rounded-lg text-primary underline"
+      >
+        Join the Tribe
+      </a>
+    </div>
+  );
+}
+
+export default JoinBtnWhite;
