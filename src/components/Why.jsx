cfdb2b1a6634d75bb0380ce7ff6d99242395adb7
olugbogi <olugbogieniola@gmail.com>
2024-10-12 13:08:37 -0700

    responsiveness

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -7,21 +7,22 @@ import combIcons from '../assets/combicons.svg'
 function why() {
   return (
     <div>
-      <div className="flex justify-center gap-20 mt-20 flex-wrap ">
-        <div className="">
-          <h1 className="font-unbounded font-normal text-5xl mb-6 mr-10">
+      <div className="flex justify-center gap-20 mt-20  sm: flex-col md:flex-col lg:flex-row">
+        <div className=" ">
+          <h1 className="font-unbounded font-normal text-5xl mb-6 mr-10 m-3 md: flex justify-center items-center">
             Why The Life <br /> Tech Hub?
           </h1>
-          <p className="font-unbounded font-normal text-xs text-zinc-600 mb-6 tracking-wide ">
+          <p className="font-unbounded font-normal text-xs text-zinc-600 mb-6 tracking-wide mr-14 m-3 md:flex justify-center items-center">
             The Life Tech Hub is more than just a place to <br /> learn tech
             skills. it’s where you grow, innovate, <br /> and make a real
             impact. Here’s why you should <br />
             choose us:
           </p>
-          <JoinBtn />
+          <div className="  sm:mr-20 m-2 md:mr-0 md:ml-48 lg:mr-64"><JoinBtn /></div>
+          
         </div>
 
-        <div className=" flex flex-col justify-center items-center gap-8">
+        <div className=" flex flex-col justify-center items-center gap-8 p-0 mr-8 md: ml-10">
           <div className="flex justify-center items-center gap-4 ">
             <div>
               <img src={majestIcons} alt="" />
